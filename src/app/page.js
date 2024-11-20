1a19b62d600f82aa7deefbce7d75eb8325811492
Chris-Wagg <cgwagg@hotmail.co.nz>
2024-11-20 12:28:40 +0100

    text change dto fix build error

--- src/app/page.js
+++ src/app/page.js
@@ -8,49 +8,49 @@ export default function Home() {
 				</div>
 				<details>
 					<summary>
-						What is Frontend Mentor, and how will it help me?
+						{`What is Frontend Mentor, and how will it help me?`}
 						<img src='icon-plus.svg' alt='' role='none' />
 					</summary>
 					<p>
-						Frontend Mentor offers realistic coding challenges to
+						{`Frontend Mentor offers realistic coding challenges to
 						help developers improve their frontend coding skills
 						with projects in HTML, CSS, and JavaScript. It's
 						suitable for all levels and ideal for portfolio
-						building.
+						building.`}
 					</p>
 				</details>
 				<details>
 					<summary>
-						Is Frontend Mentor free?
+						{`Is Frontend Mentor free?`}
 						<img src='icon-plus.svg' alt='' role='none' />
 					</summary>
 					<p>
-						Yes, Frontend Mentor offers both free and premium coding
+						{`Yes, Frontend Mentor offers both free and premium coding
 						challenges, with the free option providing access to a
-						range of projects suitable for all skill levels.
+						range of projects suitable for all skill levels.`}
 					</p>
 				</details>
 				<details>
 					<summary>
-						Can I use Frontend Mentor projects in my portfolio?
+						{`Can I use Frontend Mentor projects in my portfolio?`}
 						<img src='icon-plus.svg' alt='' role='none' />
 					</summary>
 					<p>
-						Yes, you can use projects completed on Frontend Mentor
+						{`Yes, you can use projects completed on Frontend Mentor
 						in your portfolio. It's an excellent way to showcase
-						your skills to potential employers!
+						your skills to potential employers!`}
 					</p>
 				</details>
 				<details>
 					<summary>
-						How can I get help if I'm stuck on a challenge?
+						{`How can I get help if I'm stuck on a challenge?`}
 						<img src='icon-plus.svg' alt='' role='none' />
 					</summary>
 					<p>
-						The best place to get help is inside Frontend Mentor's
+						{`The best place to get help is inside Frontend Mentor's
 						Discord community. There's a help channel where you can
 						ask questions and seek support from other community
-						members.
+						members.`}
 					</p>
 				</details>
 			</div>
